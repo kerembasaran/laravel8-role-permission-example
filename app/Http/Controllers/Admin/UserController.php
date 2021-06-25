243c58ac6a0101ace421f58ae3486a1daa5d7625
Kerem BAŞARAN <kerembasaran14@gmail.com>
2021-06-25 11:48:12 +0300

    Gates ile sayfaları koruma

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -7,6 +7,7 @@ use App\Models\Role;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
+use Illuminate\Support\Facades\Gate;
 
 class UserController extends Controller
 {
@@ -17,6 +18,9 @@ class UserController extends Controller
      */
     public function index()
     {
+        if (Gate::denies('logged-in')) {
+            dd('no access allowed');
+        }
         return view('admin.users.index', ['users' => User::paginate(10)]);
     }
 
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -16,48 +16,52 @@
 
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg">
     <div class="container">
-        <a class="navbar-brand" href="#">{{ config('app.name','User Management System') }}</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="#">{{ config('app.name', 'User Managment System') }}</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.users.index') }}">User</a>
-                </li>
-            </ul>
-            <div class="form-inline my-2 my-lg-0">
-                @if (Route::has('login'))
-                    <div>
-                        @auth
-                            <a href="{{ url('/') }}">Home</a>
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">Logout</a>
-                            <form action="{{ route('logout') }}" id="logout-form" method="POST" style="display: none">
-                                @csrf
-                            </form>
-                        @else
-                            <a href="{{ route('login') }}">Log in</a>
+        <div class="d-flex">
+            @if (Route::has('login'))
+                <div>
+                    @auth
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault();
+document.getElementById('logout-form').submit()">Logout</a>
+                        <form action="{{ route('logout') }}" id="logout-form" method="POST" style="display: none">
+                            @csrf
+                        </form>
+                    @else
+                        <a href="{{ route('login') }}">Log in</a>
 
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}">Register</a>
-                            @endif
-                        @endauth
-                    </div>
-                @endif
-            </div>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
         </div>
     </div>
 </nav>
-
+@can('logged-in')
+    <nav class="navbar sub-nav navbar-expand-lg">
+        <div class="container">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('admin.users.index') }}">Users</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+@endcan
 <main class="container">
     @include('partials.alerts')
     @yield('content')
 </main>
-
 </body>
 </html>
