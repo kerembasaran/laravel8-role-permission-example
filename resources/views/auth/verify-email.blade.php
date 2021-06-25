f986dbd555d97bce85da709a9f504a91887d0ea3
Kerem BAŞARAN <kerembasaran14@gmail.com>
2021-06-25 15:17:24 +0300

    Laravel Fortify kullanarak e-posta doğrulaması

---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,11 @@
+@extends('templates.main')
+
+@section('content')
+    <h1>Verify e-mail address</h1>
+    <p>You must verify your email address to access this page.</p>
+
+    <form action="{{ route('verification.send') }}" method="POST">
+        @csrf
+        <button type="submit" class="btn btn-primary">Resend verification email</button>
+    </form>
+@endsection
